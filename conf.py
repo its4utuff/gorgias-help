c8802528d2bd0f048c5a6151fac0608f8544a35a
Romain Lapeyre <romainplapeyre@gmail.com>
2015-06-19 13:58:28 +0200

    updated config file to take into account new name, +update in ©

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'help.gorgias.io'
-copyright = u'2014, Alexandru Plugaru'
+copyright = u'2015, Gorgias'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
